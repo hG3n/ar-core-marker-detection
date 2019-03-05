2d0ea0b7ec4fa5ecda1957fd7e5a15ff32ed7ed8
Hagen Hiller <haghiller@gmail.com>
2019-03-05 10:54:39 +0100

    Minor changes and adjustments, added functionality to get currently found markers from native plugin

--- Assets/Scripts/MarkerDetector.cs
+++ Assets/Scripts/MarkerDetector.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 using UnityEngine;
@@ -16,6 +17,9 @@ public class MarkerDetector : MonoBehaviour
     private CameraImageBytes current_image_bytes;
     private CameraIntrinsics camera_intrinsics;
 
+    private List<Marker> marker_list;
+
+
     /// <summary>
     /// Finds the markers in image.
     /// </summary>
@@ -27,7 +31,7 @@ public class MarkerDetector : MonoBehaviour
     /// <param name="pixel_buffer">Pixel buffer.</param>
     /// <param name="bufferSize">Buffer size.</param>
     [DllImport("native")]
-    private static extern void findMarkersInImage(int width, int height, int row_stride, int uv_stride, int uv_pixel_stride, byte[] pixel_buffer, int bufferSize);
+    private static extern void findMarkersInImage(int width, int height, int row_stride, byte[] pixel_buffer);
 
     [DllImport("native")]
     private static extern void SetTextureFromUnity(System.IntPtr texture, int w, int h);
@@ -38,8 +42,13 @@ public class MarkerDetector : MonoBehaviour
     [DllImport("native")]
     private static extern System.IntPtr GetRenderEventFunc();
 
+    [DllImport("native")]
+    private static extern void GetFoundMarkers(out int length, out int marker_stride, out System.IntPtr array);
+
+
     IEnumerator Start()
     {
+        //marker_list = new List<Marker>();
         CreateTextureAndPassToPlugin();
         yield return StartCoroutine("CallPluginAtEndOfFrames");
     }
@@ -60,21 +69,19 @@ public class MarkerDetector : MonoBehaviour
                 {
                     if (image.IsAvailable)
                     {
-                        _OnImageAvailable(image.Width, image.Height, image.YRowStride, image.UVRowStride, image.UVPixelStride, image.Y, 0);
-
-                        // release buffer
+                        _OnImageAvailable(image.Width, image.Height, image.YRowStride, image.Y);
                         image.Release();
                     }
                     else
                     {
-                        Debug.LogWarning("No Image seems to be available");
+                        Debug.LogWarning("There is no new image available!");
                     }
                 }
 
             }
             else
             {
-                Debug.LogWarning("AR Core session seems to be invalid");
+                Debug.LogWarning("The AR Core session seems to be invalid!");
             }
 
             // Issue a plugin event with arbitrary integer identifier.
@@ -82,6 +89,23 @@ public class MarkerDetector : MonoBehaviour
             // things it needs to do based on this ID.
             // For our simple plugin, it does not matter which ID we pass here.
             GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+
+            // gather detected markers from native plugin
+            _GetMarkersFromPlugin();
+
+
+            //// Raycast for each marker location to find the three dimensional references
+            //TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            //    TrackableHitFlags.FeaturePointWithSurfaceNormal;
+            //foreach (var marker in marker_list)
+            //{
+            //    var centroid = marker.GetCentroid();
+
+            //    TrackableHit hit;
+            //    Frame.Raycast(centroid.x, centroid.y, raycastFilter, out hit);
+            //    Debug.Log("Latest hit: " + hit);
+            //}
+
         }
     }
 
@@ -110,7 +134,7 @@ public class MarkerDetector : MonoBehaviour
     /// <param name="rowStride">Row stride.</param>
     /// <param name="pixel_buffer">Pixel buffer.</param>
     /// <param name="buffer_size">Buffer size.</param>
-    private void _OnImageAvailable(int width, int height, int rowStride, int uvStride, int uvPixelStride, System.IntPtr pixel_buffer, int buffer_size)
+    private void _OnImageAvailable(int width, int height, int rowStride, System.IntPtr pixel_buffer)
     {
         // Adjust buffer size if necessary.
         int actual_bugger_size = rowStride * height;
@@ -120,7 +144,7 @@ public class MarkerDetector : MonoBehaviour
         Marshal.Copy(pixel_buffer, image_buffer, 0, actual_bugger_size);
         try
         {
-            findMarkersInImage(width, height, rowStride, uvStride, uvPixelStride, image_buffer, actual_bugger_size);
+            findMarkersInImage(width, height, rowStride, image_buffer);
         }
         catch (System.Exception e)
         {
@@ -128,5 +152,44 @@ public class MarkerDetector : MonoBehaviour
             Debug.LogError(e);
         }
 
+    }
+
+    /// <summary>
+    /// Gets the markers from plugin.
+    /// </summary>
+    private void _GetMarkersFromPlugin()
+    {
+        // clear local marker list
+        //marker_list.Clear();
+
+        // create local variables & gather values
+        int length;
+        int marker_stride;
+        System.IntPtr marker_array_ptr;
+        GetFoundMarkers(out length, out marker_stride, out marker_array_ptr);
+        byte[] marker_array = new byte[length];
+
+        // copy data
+        Marshal.Copy(marker_array_ptr, marker_array, 0, length);
+        Marshal.FreeCoTaskMem(marker_array_ptr);
+
+        int num_markers = length / marker_stride;
+        Debug.Log("Number of markers: " + num_markers);
+
+        if (length > 0)
+        {
+            for (int n = 0; n < num_markers; ++n)
+            {
+                int id = (int)marker_array[n];
+                int direction = (int)marker_array[n + 1];
+                Vector2 tl = new Vector2(marker_array[n + 2], marker_array[n + 3]);
+                Vector2 tr = new Vector2(marker_array[n + 4], marker_array[n + 5]);
+                Vector2 br = new Vector2(marker_array[n + 6], marker_array[n + 7]);
+                Vector2 bl = new Vector2(marker_array[n + 8], marker_array[n + 9]);
+                //Marker m = new Marker(id, direction, tl, tr, br, bl);
+
+                //this.marker_list.Add(m);
+            }
+        }
     }
 }
